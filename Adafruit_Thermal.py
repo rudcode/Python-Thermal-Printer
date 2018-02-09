b7a0808678f7146b893a0913476f43cae75dac9c
Derek Olson <derek@derek-o.com>
2018-02-09 14:41:43 -0600

    Add implementation of DTR handshake

--- Adafruit_Thermal.py
+++ Adafruit_Thermal.py
@@ -37,6 +37,11 @@ import time
 import sys
 import math
 
+try:
+	import RPi.GPIO as GPIO
+except:
+	print('Must run on Raspberry Pi for DTR Handshake')
+
 class Adafruit_Thermal(Serial):
 
 	resumeTime      =   0.0
@@ -58,6 +63,9 @@ class Adafruit_Thermal(Serial):
 		# NEW BEHAVIOR: if no parameters given, output is written
 		# to stdout, to be piped through 'lp -o raw' (old behavior
 		# was to use default port & baud rate).
+		self.dtrEnabled = False
+		self.dtrPin = None
+
 		baudrate = 19200
 		if len(args) == 0:
 			self.writeToStdout = True
@@ -67,6 +75,10 @@ class Adafruit_Thermal(Serial):
 		elif len(args) == 2:
 			# If both passed, use those values.
 			baudrate = args[1]
+
+		elif len(args) == 3:
+			self.dtrPin = args[2]
+			args = [args[0], args[1]]
 
 		# Firmware is assumed version 2.68.  Can override this
 		# with the 'firmware=X' argument, where X is the major
@@ -133,6 +145,15 @@ class Adafruit_Thermal(Serial):
 			  18, # DC2
 			  35, # Print density
 			  (printBreakTime << 5) | printDensity)
+
+			# Enable DTR pin if requested
+			if(self.dtrPin):
+				GPIO.setmode(GPIO.BCM)
+				GPIO.setup(self.dtrPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+				self.writeBytes(29, 97, (1 << 5))
+				self.dtrEnabled = True
+				print('Printer DTR Handshake Enabled')
+
 			self.dotPrintTime = 0.03
 			self.dotFeedTime  = 0.0021
 		else:
@@ -152,12 +173,16 @@ class Adafruit_Thermal(Serial):
 
 	# Sets estimated completion time for a just-issued task.
 	def timeoutSet(self, x):
-		self.resumeTime = time.time() + x
+		if not self.dtrEnabled:
+			self.resumeTime = time.time() + x
 
 	# Waits (if necessary) for the prior task to complete.
 	def timeoutWait(self):
 		if self.writeToStdout is False:
-			while (time.time() - self.resumeTime) < 0: pass
+			if self.dtrEnabled:
+				while (GPIO.input(self.dtrPin) == GPIO.HIGH): pass
+			else:
+				while (time.time() - self.resumeTime) < 0: pass
 
 	# Printer performance may vary based on the power supply voltage,
 	# thickness of paper, phase of the moon and other seemingly random
